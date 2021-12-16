19bdbb01efb0351e7b866f0efd33e81e64b99cf4
Etentuk <etentuke@gmail.com>
2021-12-16 01:10:05 +0100

    completed user authentication

--- bugtracker/views.py
+++ bugtracker/views.py
@@ -3,7 +3,13 @@ from django.db import IntegrityError
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
-from django.contrib.auth.decorators import login_required
+from django.db.models.query_utils import Q
+from django.http import HttpResponse
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.template.loader import render_to_string
+from django.core.mail import send_mail, BadHeaderError
 
 
 from .models import User
@@ -32,11 +38,11 @@ def login_view(request):
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
         else:
-            return render(request, "network/login.html", {
+            return render(request, "bugtracker/authentication/login.html", {
                 "message": "Invalid username and/or password."
             })
     else:
-        return render(request, "bugtracker/login.html")
+        return render(request, "bugtracker/authentication/login.html")
 
 
 def logout_view(request):
@@ -53,7 +59,7 @@ def register(request):
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
-            return render(request, "bugtracker/register.html", {
+            return render(request, "bugtracker/authentication/register.html", {
                 "message": "Passwords must match."
             })
 
@@ -63,10 +69,37 @@ def register(request):
             user.isDeveloper()
             user.save()
         except IntegrityError:
-            return render(request, "bugtracker/register.html", {
+            return render(request, "bugtracker/authentication/register.html", {
                 "message": "Username already taken."
             })
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
-        return render(request, "bugtracker/register.html")
+        return render(request, "bugtracker/authentication/register.html")
+
+
+def password_reset_request(request):
+    if request.method == "POST":
+        data = request.POST['email']
+        associated_users = User.objects.filter(Q(email=data))
+        if associated_users.exists():
+            for user in associated_users:
+                subject = "Password Reset Requested"
+                email_template_name = "bugtracker/authentication/password_reset_email.txt"
+                c = {
+                    "email": user.email,
+                    'domain': '127.0.0.1:8000',
+                    'site_name': 'Website',
+                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                    "user": user,
+                    'token': default_token_generator.make_token(user),
+                    'protocol': 'http',
+                }
+                email = render_to_string(email_template_name, c)
+                try:
+                    send_mail(subject, email, 'reset@bugtracker.com',
+                              [user.email], fail_silently=False)
+                except BadHeaderError:
+                    return HttpResponse('Invalid header found.')
+        return redirect("/password_reset/done/")
+    return render(request=request, template_name="bugtracker/authentication/password_reset.html")
